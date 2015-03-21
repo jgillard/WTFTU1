fd00cc6134204465a1329621eb17a258b394f344
James Gillard <jamesgillard@live.co.uk>
2015-03-21 00:18:56 +0000

    Before-bed commit

--- WTFTU1.py
+++ WTFTU1.py
@@ -6,20 +6,15 @@ app = Flask(__name__)
 @app.route('/')
 def start():
     dt = datetime.datetime.now()
-    DoW = int(dt.strftime("%w"))
-    time24 = int(dt.strftime("%H%M"))
-    if DoW == 0:
-        return "Error: 1"  # uniSun
-    elif DoW == 6:
-        return "Error: 2"  # uniSat
-    else:
-        while time24 < 2400:
-            if time24 not in uniWeekday:
-                time24 += 1
+    day = int(dt.strftime("%w"))
+    time = int(dt.strftime("%H%M"))
+    while time < 2400:
+            if time not in TIMETABLE_DAY[day]:
+                time += 1
             else:
-                response = (str(time24)[0:2] + ":" + str(time24)[2:4])
+                response = (str(time)[0:2] + ":" + str(time)[2:4])
                 return response
-        return "No Bus Time"
+    return "No Bus Time"
 
 
 uniWeekday = [1708, 1738, 1808, 1838, 1914, 2014, 2114, 2239]
@@ -28,6 +23,9 @@ uniSun = []
 leamWeekday = [1743, 1813, 1841, 1911, 1943, 2043, 2143, 2308]
 leamSat = []
 leamSun = []
+
+TIMETABLE_DAY = [uniWeekday] * 5
+TIMETABLE_DAY.extend([uniSat, uniSun])
 
 
 if __name__ == '__main__':
