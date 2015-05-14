6901cdc3449e3578acc033b2382594df5dc5e909
James Gillard <jamesgillard@live.co.uk>
2015-05-14 22:44:29 +0100

    Refactored day handling

--- WTFTU1.py
+++ WTFTU1.py
@@ -14,25 +14,17 @@ def findTime(timetable):
     day = int(dt.strftime("%w"))
     time = int(dt.strftime("%H%M"))
     timeTemp = time
+    if timeTemp < 400:
+        day -= 1
     for timeTemp in range(time, 2400):
-        if timeTemp > 400:
-            if timeTemp in timetable[day]:
-                index = timetable[day].index(timeTemp)
-                nextBus = timetable[day][index]
-                if nextBus != timetable[day][-1]:
-                    nextnextBus = timetable[day][index+1]
-                else:
-                    nextnextBus = 9999
-                return [formatTime(nextBus), formatTime(nextnextBus)]
-        else:
-            if timeTemp in timetable[day-1]:
-                index = timetable[day].index(timeTemp)
-                nextBus = timetable[day][index]
-                if timetable[day][index] != timetable[day][-1]:
-                    nextnextBus = timetable[day][index+1]
-                else:
-                    nextnextBus = 9999
-                return [formatTime(nextBus), formatTime(nextnextBus)]
+        if timeTemp in timetable[day]:
+            index = timetable[day].index(timeTemp)
+            nextBus = timetable[day][index]
+            if nextBus != timetable[day][-1]:
+                nextnextBus = timetable[day][index+1]
+            else:
+                nextnextBus = 9999
+            return [formatTime(nextBus), formatTime(nextnextBus)]
     return "No buses you moron"
 
 
