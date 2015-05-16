f0610cc28ec71e155e3143b97ed7bec80c622437
James Gillard <jamesgillard@live.co.uk>
2015-05-16 10:09:56 +0100

    Functions, grabs new times periodically
    Currently queries the server once per minute. TODO: check whether bus time has passed and only query if it has.

--- src/app.js
+++ src/app.js
@@ -1,7 +1,42 @@
 var UI = require('ui');
 var Vector2 = require('vector2');
 var ajax = require('ajax');
-var Light = require('ui/light');
+
+function getLoc() {
+  function locationSuccess(pos) {
+    lat = pos.coords.latitude;
+    long = pos.coords.longitude;
+    console.log('lat= ' + lat + ' lon= ' + long);
+  }
+  function locationError(err) {
+    console.log('location error (' + err.code + '): ' + err.message);
+  }
+  navigator.geolocation.getCurrentPosition(locationSuccess, locationError, {maximumAge: 600000, timeout: 5000});
+}
+
+function getBus() {
+  ajax(
+    {
+      url: 'https://wtftu1.herokuapp.com',
+      type: 'json'
+    },
+    function(data) {
+      if(lat < 52.33 && lat > 52.23 & long < -1.50 && long > -1.60) { 
+        card.subtitle('Parade: ' + data.info.toUni + '\n2nd: ' + data.info.toUni2); // in Leam
+        card.body('But they probably won\'t turn up');
+      } else if(lat < 52.40 && lat > 52.37 & long < -1.54 && long > -1.68) { 
+        card.subtitle('AL3: ' + data.info.toLeam + '\n2nd: ' + data.info.toLeam2); // at Uni
+        card.body('But they probably won\'t turn up');
+      } else {
+        card.subtitle('Where the frack are you?');
+        card.body('What were you expecting?');
+      }
+    },  
+    function(error) {
+      console.log('Failed fetching bus data: ' + error);
+    }
+  );
+}
 
 // SPLASH SCREEN
 var splash = new UI.Window({ fullscreen: true });
@@ -12,52 +47,35 @@ var image = new UI.Image ({
 });
 splash.add(image);
 splash.show();
-Light.on();
 
 // GET LOCATION
-var lat;
-var long;
-function locationSuccess(pos) {
-  lat = pos.coords.latitude;
-  long = pos.coords.longitude;
-  console.log('lat= ' + lat + ' lon= ' + long);
-}
-function locationError(err) {
-  console.log('location error (' + err.code + '): ' + err.message);
-}
-navigator.geolocation.getCurrentPosition(locationSuccess, locationError, {maximumAge: 600000, timeout: 5000});
+getLoc();
 
+// MAKE MAIN SCREEN
 var card = new UI.Card({
   title:'WTFTU1',
   subtitle:'Summoning...'
 });
 
-// MAIN SCREEN
+// SHOW MAIN SCREEN
 setTimeout(function() {
-  Light.auto();
   card.show();
-  splash.hide(); // Hide the splash screen to avoid showing it when the user press Back.
-}, 3000);
+  splash.hide();
+}, 2000);
 
 // GET TIMETABLE DATA
-ajax(
-  {
-    url: 'https://wtftu1.herokuapp.com',
-    type: 'json'
-  },
-  function(data) {
-    if(lat < 52.33 && lat > 52.23 & long < -1.50 && long > -1.60) { 
-      card.subtitle('To Uni: ' + data.info.toUni); // in Leam
-      card.body('But it probably won\'t turn up');
-    } else if(lat < 52.40 && lat > 52.37 & long < -1.54 && long > -1.68) { 
-      card.subtitle('To Leam: ' + data.info.toLeam); // at Uni
-      card.body('But it probably won\'t turn up');
-    } else {
-      card.subtitle('Where the frack are you?');
-      card.body('What were you expecting?');
-    }
-  },  
-  function(error) {
-    console.log('Failed fetching weather data: ' + error);
-  }
-);
+getBus();
+
+// SEE THE SPLASH ANYTIME!
+card.on('click', 'select', function() {
+  splash.show();
+});
+
+// UPDATE BY THE MINUTE
+function update() {
+  setTimeout(function() {
+    getBus();
+    update();
+  }, 60000);
+}
+update();
